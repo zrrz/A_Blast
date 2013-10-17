69ef762ee3bfaaeae7f779edaae5b0b4a34dd97c
Zachary Rock <zacharyrock@hotmail.com>
2013-10-17 15:56:55 -0700

    Enemy ship.

---
+++ Assets/EnemyShipMove.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyShipMove : MonoBehaviour {
+
+	public float maxHealth = 5;
+	
+	float health;
+	
+	Transform thisTransform;
+	
+	public float spinSpeed = 25.0f;
+	
+	void Start () {
+		health = maxHealth;
+		thisTransform = transform;
+	}
+	
+	void Update () {
+		thisTransform.Rotate(Vector3.up, spinSpeed*Time.deltaTime);
+	}
+	
+	void OnCollisionEnter(Collision col) {
+		if(col.gameObject.tag == "PlayerBullet") {
+			health--;
+			if(health <= 0) {
+				Destroy(gameObject);
+			}
+			Destroy(col.gameObject);
+		}
+	}
+}
