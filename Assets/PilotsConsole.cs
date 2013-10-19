61f5a755518f2386aadff130d5c9c991168c54fe
Zachary Rock <zacharyrock@hotmail.com>
2013-10-18 19:39:25 -0700

    Seperated shipmove into shipmove and pilotconsole. Fixed collision and centered some things on ship.

---
+++ Assets/PilotsConsole.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PilotsConsole : MonoBehaviour {
+
+	BaseInput input;	
+	PlayerMove player;
+	
+	bool used = false;
+	bool playerNear = false;
+	
+	CameraFollow cameraFollow;
+	
+	public float camSize = 17.0f;
+	public Transform camAnchor;
+	
+	public ShipMove ship;
+	
+	void Start () {
+		cameraFollow = Camera.main.GetComponent<CameraFollow>();
+		player = GameObject.Find("Player").GetComponent<PlayerMove>();
+		input = player.GetComponent<PlayerInput>();
+		
+		if(ship == null)
+			ship = transform.parent.GetComponent<ShipMove>();
+		
+	}
+	
+	void Update () {
+		if(playerNear) {
+			if(Input.GetKeyDown(KeyCode.E)) {
+				used = !used;
+				player.used = !player.used;
+				if(used)
+					cameraFollow.ChangeCam(camAnchor, camSize);
+				else
+					cameraFollow.Reset();
+			}
+		}
+		if(used) {
+			ship.Move(input.dir);
+		}
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.tag == "Player") {
+			playerNear = true;
+		}
+	}
+	
+	void OnTriggerExit(Collider other) {
+		if(other.gameObject.tag == "Player") {
+			playerNear = false;
+		}
+	}
+	
+	void OnGUI() {
+		if(playerNear) {
+			GUI.Box(new Rect(0.0f, 0.0f, 150.0f, 50.0f), "Press 'E' to enter");
+		}
+		
+		if(used)  {
+			GUI.Box(new Rect(0.0f, Screen.height - 40.0f, 150.0f, 40.0f), "Press 'LMB' to fire\n'WASD' to move");
+		}
+	}
+}
--- Assets/ShipMove.cs
+++ Assets/ShipMove.cs
@@ -3,64 +3,38 @@ using System.Collections;
 
 public class ShipMove : MonoBehaviour {
 	
-	Transform shipTransform;
-	BaseInput input;	
-	PlayerMove player;
+	Transform thisTransform;
+	Rigidbody thisRigidbody;
 	
 	public float speed = 5.0f;
 	public float turnSpeed = 5.0f;
 	
-	bool used = false;
-	bool playerNear = false;
+	Vector3 targetVelocity;
+	//Vector3 targetAngularVelocity;
 	
-	CameraFollow cameraFollow;
+	public float maxVelocityChange = 10.0f;
+	//public float maxAngularVelocityChange = 10.0f;
 	
-	public float camSize = 17.0f;
-	public Transform camAnchor;
-	
-	void Start () {
-		cameraFollow = Camera.main.GetComponent<CameraFollow>();
-		shipTransform = transform.parent;
-		player = GameObject.Find("Player").GetComponent<PlayerMove>();
-		input = player.GetComponent<PlayerInput>();
+	void Start () {	
+		thisTransform = transform;
+		thisRigidbody = rigidbody;
 	}
 	
-	void Update () {
-		if(playerNear) {
-			if(Input.GetKeyDown(KeyCode.E)) {
-				used = !used;
-				player.used = !player.used;
-				if(used)
-					cameraFollow.ChangeCam(camAnchor, camSize);
-				else
-					cameraFollow.Reset();
-			}
-		}
-		if(used) {
-			shipTransform.position += shipTransform.TransformDirection(0.0f, 0.0f, input.dir.z * speed * Time.deltaTime);
-			shipTransform.Rotate(Vector3.up, input.dir.x * turnSpeed * Time.deltaTime);	
-		}
+	public void Move(Vector3 dir) {
+		targetVelocity = thisTransform.TransformDirection(0.0f, 0.0f, dir.z * speed);
+		//targetAngularVelocity = new Vector3(0.0f, dir.x * turnSpeed, 0.0f);
+		thisTransform.Rotate(Vector3.up, dir.x * turnSpeed * Time.deltaTime);
 	}
 	
-	void OnTriggerEnter(Collider other) {
-		if(other.gameObject.tag == "Player") {
-			playerNear = true;
-		}
-	}
-	
-	void OnTriggerExit(Collider other) {
-		if(other.gameObject.tag == "Player") {
-			playerNear = false;
-		}
-	}
-	
-	void OnGUI() {
-		if(playerNear) {
-			GUI.Box(new Rect(0.0f, 0.0f, 150.0f, 50.0f), "Press 'E' to enter");
-		}
+	void FixedUpdate() {
+		Vector3 velocityChange = targetVelocity - thisRigidbody.velocity;
+		velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+		thisRigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
 		
-		if(used)  {
-			GUI.Box(new Rect(0.0f, Screen.height - 40.0f, 150.0f, 40.0f), "Press 'LMB' to fire\n'WASD' to move");
-		}
+		thisRigidbody.angularVelocity = Vector3.zero;
+		
+		//Vector3 angularVelocityChange = targetAngularVelocity - thisRigidbody.angularVelocity;
+		//angularVelocityChange.y = Mathf.Clamp(angularVelocityChange.y, -maxAngularVelocityChange, maxAngularVelocityChange);
+		//thisRigidbody.AddTorque(angularVelocityChange, ForceMode.VelocityChange);
 	}
 }
