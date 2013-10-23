e2564e545d69b573334cc7876f016ac00e274f3e
Zachary Rock <zacharyrock@hotmail.com>
2013-10-23 04:59:47 -0700

    Win scene. Trigger boxes on player ship and space station. PlayerShip tag.

---
+++ Assets/GameOverZone.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverZone : MonoBehaviour {	
+	void OnTriggerEnter(Collider col) {
+		if(col.gameObject.tag == "PlayerShip") {
+			Application.LoadLevel("Win");
+		}
+	}
+}
